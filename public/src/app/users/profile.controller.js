b912cbf724cd93a7e17bef9765d5384eb7d84386
Unknown <1412548@student.hcmus.edu.vn>
2017-06-29 01:21:59 +0700

    Add comment in product detail, up/down vote in user profile, recaptcha

--- public/src/app/users/profile.controller.js
+++ public/src/app/users/profile.controller.js
@@ -10,11 +10,26 @@
     function ProfileController($scope) {
         var vm = this;
 
-
+        $scope.upVote = false;
+        $scope.downVote = false;
         activate();
 
         ////////////////
 
         function activate() {}
+
+        $scope.changeOption = function(value) {
+            if (value == '1') {
+                $scope.upVote = !$scope.upVote;
+                if ($scope.downVote) {
+                    $scope.downVote = !$scope.downVote;
+                }
+            } else if (value == '2') {
+                $scope.downVote = !$scope.downVote;
+                if ($scope.upVote) {
+                    $scope.upVote = !$scope.upVote;
+                }
+            }
+        }
     }
 })();
