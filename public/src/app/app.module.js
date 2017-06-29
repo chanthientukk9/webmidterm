da384ecfe7d5dbdca87b78085ec606d53a6a39cd
Unknown <1412548@student.hcmus.edu.vn>
2017-06-29 10:45:28 +0700

    header when signed in

--- public/src/app/app.module.js
+++ public/src/app/app.module.js
@@ -2,6 +2,6 @@
     'use strict';
 
     angular.module('app', [
-        'app.core', 'app.product', 'app.users', 'ngAnimate', 'ui.bootstrap', 'ngCookies'
+        'app.core', 'app.product', 'app.users', 'ngAnimate', 'ui.bootstrap', 'ngCookies', 'vcRecaptcha'
     ]);
 })();
