a45221e2e19e2ea34c74f23e1aacb4fe53477d02
Unknown <1412548@student.hcmus.edu.vn>
2017-11-25 15:44:44 +0700

    add google maps api to location and status page

--- public/src/app/booking/location/location.controller.js
+++ public/src/app/booking/location/location.controller.js
@@ -17,6 +17,14 @@
         function activate() { 
 
         }
+        var map;
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+        }
+        initMap();
 
     }
 })();
--- public/src/app/booking/status/status.controller.js
+++ public/src/app/booking/status/status.controller.js
@@ -15,5 +15,13 @@
         ////////////////
 
         function activate() { }
+        var map;
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+        }
+        initMap();
     }
 })();
