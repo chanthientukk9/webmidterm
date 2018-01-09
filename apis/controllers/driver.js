146c2786f7efdf5468200e12bce8b0c1fd6ac3f2
Unknown <chanthientukk9@gmail.com>
2018-01-09 11:44:06 +0700

    Update api reply invitation

--- apis/controllers/driver.js
+++ apis/controllers/driver.js
@@ -1,5 +1,7 @@
 var firebase = require('firebase-admin');
 var driverRef = firebase.database().ref().child('drivers');
+var customerRef = firebase.database().ref().child('customers');
+
 const RADIAN = 0.015; // 0.015
 var helpers = require('../modules/helpers.js');
 var md5 = require('js-md5');
@@ -150,6 +152,76 @@ module.exports.createDriver = function(req, res, next) {
             message: 'Can not create new driver'
         });
     });
+}
+
+module.exports.replyInvitation = function(req, res, next) {
+    var driverData = {
+        lat: req.body.lat,
+        lng: req.body.lng,
+        carType: req.body.carType,
+        status: req.body.status,
+        customer: req.body.customer,
+        timestamp: req.body.timestamp
+    };
+    if(driverData.status === 'deny') {
+        let customer = {};
+        customerRef.orderByKey().equalTo(req.body.customer).once('value', function(snapshot) {
+            snapshot.forEach(function(element) {
+                var data = {
+                    id: element.key,
+                    value: element.val()
+                }
+                customer = data;
+            })
+        }).then(function(){
+            var customerData = {
+                address: customer.value.address,
+                lat: customer.value.lat,
+                lng: customer.value.lng,
+                carType: customer.value.carType,
+                status: 'pending',
+                driver: customer.value.driver,
+                timestamp: customer.value.timestamp
+            };
+            var updates = {};
+            updates['/' + customer.id] = customerData;
+            return customerRef.update(updates).then(function(customer) {
+                var updates = {};
+                driverData.status = 'waiting';
+                updates['/' + req.userData._id] = driverData;
+                driverRef.update(updates).then(function(driver) {
+                    return res.status(200).json({
+                        message: 'Success'
+                    })
+                }).catch(function(err) {
+                    return res.status(500).json({
+                        message: 'Can not update driver'
+                    });
+                });
+            }).catch(function(err) {
+                return res.status(500).json({
+                    message: 'Can not update customer'
+                });
+            });
+        }).catch(function(err) {
+            return res.status(500).json({
+                message: 'Cannot get customers'
+            });
+        });
+    } else {
+        driverData.status = 'picking'
+        var updates = {};
+        updates['/' + req.userData._id] = driverData;
+        driverRef.update(updates).then(function(driver) {
+            return res.status(200).json({
+                message: 'Success'
+            })
+        }).catch(function(err) {
+            return res.status(500).json({
+                message: 'Can not update driver'
+            });
+        });
+    }
 }
 
 module.exports.updateDriver = function(req, res, next) {
