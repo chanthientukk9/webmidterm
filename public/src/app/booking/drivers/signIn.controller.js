5d742239c25f95fe6cf47e93a5d7de5db7a093da
ngdinhtien <1412548@student.hcmus.edu.vn>
2018-01-09 11:01:38 +0700

    signIn controller for login

---
+++ public/src/app/booking/drivers/signIn.controller.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.booking')
+        .controller('SignInController1', SignInController1);
+
+    SignInController1.inject = ['$scope', 'BookingService', 'Dialog', '$cookies', '$uibModalInstance', '$rootScope'];
+
+    function SignInController1($scope, BookingService, Dialog, $cookies, $uibModalInstance, $rootScope) {
+        var vm = this;
+
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+        }
+
+        $scope.login = function login() {
+            BookingService.LoginDriver($scope.user).then(function(res) {
+                Dialog.Success("Thành công");
+
+                var expire = new Date();
+                expire.setHours(expire.getHours() + 6);
+                $cookies.put('tkcc', res.token, { path: '/', expries: expire });
+
+                $uibModalInstance.close(res.data);
+            }, function(err) {
+                Dialog.Error("Lỗi", err.data.message);
+            })
+        }
+    }
+})();
